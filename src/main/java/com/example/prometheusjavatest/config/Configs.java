ffb4c2b2dafb283cc5a67f526cb2ec3caac2bbed
YuJian <754369677@qq.com>
2021-06-18 14:15:27 +0800

    first commit

---
+++ src/main/java/com/example/prometheusjavatest/config/Configs.java
@@ -0,0 +1,17 @@
+package com.example.prometheusjavatest.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configs {
+    @Bean
+    MeterRegistryCustomizer meterRegistryCustomizer(MeterRegistry meterRegistry) {
+        return meterRegistry1 -> {
+            meterRegistry.config()
+                .commonTags("application", "test");
+        };
+    }
+}
